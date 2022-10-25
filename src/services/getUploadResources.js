0734279f0506c0faedc1a66fcf7736c0534d2a05
Agustin Avila <agustin.avila.dev@gmail.com>
2022-10-25 12:38:44 -0300

    feat: add resize transform & adapt response

--- src/services/getUploadResources.js
+++ src/services/getUploadResources.js
@@ -1,5 +1,15 @@
 import cloudinary from '../config/cloudinary.js';
-import { logger } from '../utils/logger.js';
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+const cloudName = process.env.CLOUDINARY_NAME;
+const BASE_URL = `https://res.cloudinary.com/${cloudName}/image/upload`;
+const TRANSF_W400 = 'c_scale,w_400';
+
+const addTransformation = (imgUrl, baseUrl, transform) => {
+  const splited = imgUrl.split(baseUrl);
+  return `${baseUrl}/${transform}${splited[1]}`;
+};
 
 const getUploadResources = async (folder, limit, nextCursor) => {
   try {
@@ -11,17 +21,28 @@ const getUploadResources = async (folder, limit, nextCursor) => {
     };
 
     const results = await cloudinary.api.resources(options);
+
     const response = {
       nextCursor: results.next_cursor,
       images: results.resources
         .map((item) => {
+          const w400 = addTransformation(
+            item.secure_url,
+            BASE_URL,
+            TRANSF_W400
+          );
           return {
-            assetId: item.asset_id,
-            publicId: item.public_id,
-            url: item.secure_url,
-            format: item.format,
-            width: item.width,
-            height: item.height
+            original: {
+              assetId: item.asset_id,
+              publicId: item.public_id,
+              url: item.secure_url,
+              format: item.format,
+              width: item.width,
+              height: item.height
+            },
+            transform: {
+              w400
+            }
           };
         })
         .sort((a, b) => a.publicId - b.publicId)
