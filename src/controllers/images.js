362a3d37ae5fa2bda67a1e6ed9c29e0a65aef478
Agustin Avila <agustin.avila.dev@gmail.com>
2022-10-25 14:38:01 -0300

    feat: todo notes, image resize on upload

--- src/controllers/images.js
+++ src/controllers/images.js
@@ -3,6 +3,12 @@ import { imageRemove } from '../services/imageRemove.js';
 import { getUploadResources } from '../services/getUploadResources.js';
 
 const upload = async (req, res, next) => {
+  // TODO: image resize
+  // 1080 px max width OR heigth
+  // if <, do nothing
+  // https://sharp.pixelplumbing.com/api-resize
+  // https://github.com/lovell/sharp/blob/main/docs/api-resize.md
+
   try {
     const { files } = req;
     const { folder } = req.body;
